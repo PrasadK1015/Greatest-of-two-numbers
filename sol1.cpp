79d6580d27af43f7b532f201d5c994f0a476fb8b
PrasadK1015 <prasad.kadam1015@gmail.com>
2024-06-30 12:16:58 +0530

    Add files via upload

---
+++ sol1.cpp
@@ -0,0 +1,20 @@
+             //GREATEST OF TWO NUMBERS
+
+# include <iostream>
+using namespace std;
+int main(){
+
+             //GREATEST OF TWO NUMBERS
+int x,y;
+cin>>x;
+cin>>y;
+cout<<"Value of x is :- "<<x<<" and value of y is :- "<<y<<endl;
+if(x>y){
+    cout<<"X is greater than Y"<<endl;
+}else{
+    cout<<"Y is greater than X"<<endl;
+};
+
+
+
+};
